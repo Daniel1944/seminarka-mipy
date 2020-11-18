09909e259125e69801ac15fdc087c84a22ca3a88
Daniel1944 <daniel.jantulik@ssinfis.cz>
2020-11-18 21:46:52 +0100

    edit

--- seminarka.py
+++ seminarka.py
@@ -1,50 +1,60 @@
+import time
+import os
+
+
 def bit_to_mb(n):
-    # zjistení o jaký vstup se jedná list, tuple, int
     if isinstance(n, list or n, tuple):
         mbList = []
         for x in range(len(n)):
             tmp = n[x]
             tmp = (1023.0 ** -2) * tmp
             mbList.append(tmp)
-            tmp = 0
         return mbList
     else:
         return (1023.0 ** -2) * n
 
 
 def ex2(information):
-    current_year = 2020
+    # pretvoreni tuplu do retezce pomoci specifikace
+    current_year: str = time.strftime("%Y")
     output = []
     tmp = information.strip("[]")
     input = tmp.replace("}", "}|").replace(", {", "{").replace('"', "").replace('{', "").replace('}', "").split("|")
     del input[-1]
-
     for i in range(len(input)):
         tmp = str(input[i])
-        inforArr = tmp.split(",")
+        info_arr = tmp.split(",")
         sentences = []
-        for j in range(len(inforArr)):
-            if inforArr[j] == "Muz":
+        for j in range(len(info_arr)):
+            if info_arr[j] == "Muz":
                 sentences.append("Pan ")
-            elif inforArr[j] == "Zena":
+            elif info_arr[j] == "Zena":
                 sentences.append("Paní ")
             if j == 1:
-                tmp = inforArr[1].split(" ")
+                tmp = info_arr[1].split(" ")
                 sentences.append(tmp[2] + " " + tmp[1] + ", ")
-            if j == 2 and inforArr[2] != " ":
-                x = int(inforArr[2])
-                age = current_year - x
+            if j == 2 and info_arr[2] != " ":
+                date_of_birth = int(info_arr[2])
+                age = (int(current_year)) - date_of_birth
                 sentences.append("ve věku " + str(age) + " let.")
-            elif j == 2 and inforArr[2] == ' ':
+            elif j == 2 and info_arr[2] == ' ':
                 sentences.append("ve věku XXX let.")
-            if j == 3 and inforArr[3] != " ":
-                sentences.append("bytem " + inforArr[3])
-            elif j == 3 and inforArr[3] == ' ':
+            if j == 3 and info_arr[3] != " ":
+                sentences.append("bytem " + info_arr[3])
+            elif j == 3 and info_arr[3] == ' ':
                 sentences.append("bytem neznámé, ")
         output.append(sentences)
     return output
 
 
-x = '[{"Muz", "Novy Karel", "1950", "Praha"}, {"Zena", "Hladova Jana", "1992", ""}, {"Muz", "Janek Jan", "", "Ostrava"}]'
+def get_me_info():
+   side = 5
+   range = 7
+   for i in range(side):
+       for j in range(range):
+               print("*", end=' ')
+       print()
 
-print(ex2(x))
+
+
+get_me_info()
